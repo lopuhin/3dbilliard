30b6c1857f5404646b09b2cb69b46d4f7f46117f
lopuhin <kostia.lopuhin@gmail.com>
2011-02-15 21:41:46 +0300

    draw the green rect and rotate it

--- billiard.js
+++ billiard.js
@@ -9,43 +9,25 @@ function start() {
     gl.enable(gl.DEPTH_TEST);
     gl.depthFunc(gl.LEQUAL);
 
+    document.onkeydown = handleKeyDown;
+    document.onkeyup = handleKeyUp;
+    
     setInterval(tick, 15);
 }
 
-var triangleVertexPositionBuffer;
-var triangleVertexColorBuffer;
 var squareVertexPositionBuffer;
 var squareVertexColorBuffer;
 
-function initBuffers() {
-    triangleVertexPositionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
-    var vertices = [
-        0.0,  1.0,  0.0,
-       -1.0, -1.0,  0.0,
-        1.0, -1.0,  0.0];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    triangleVertexPositionBuffer.itemSize = 3;
-    triangleVertexPositionBuffer.numItems = 3;
+var table_max_x = 2, table_max_z = 3;
 
-    triangleVertexColorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexColorBuffer);
-    var colors = [
-        1.0, 0.0, 0.0, 1.0,
-        0.0, 1.0, 0.0, 1.0,
-        0.0, 0.0, 1.0, 1.0
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-    triangleVertexColorBuffer.itemSize = 4;
-    triangleVertexColorBuffer.numItems = 3;
-    
+function initBuffers() {
     squareVertexPositionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
-    vertices = [
-        1.0,  1.0,  0.0,
-       -1.0,  1.0,  0.0,
-        1.0, -1.0,  0.0,
-       -1.0, -1.0,  0.0
+    var vertices = [
+        table_max_x,  0.0,  table_max_z,
+       -table_max_x,  0.0,  table_max_z,
+        table_max_x,  0.0, -table_max_z,
+       -table_max_x,  0.0, -table_max_z
     ];
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     squareVertexPositionBuffer.itemSize = 3;
@@ -53,17 +35,17 @@ function initBuffers() {
 
     squareVertexColorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexColorBuffer);
-    colors = [];
+    var colors = [];
     for (var i=0; i < 4; i++) {
-      colors = colors.concat([0.5, 0.5, 1.0, 1.0]);
+      colors = colors.concat([0.0, 0.5, 0.0, 1.0]);
     }
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
     squareVertexColorBuffer.itemSize = 4;
     squareVertexColorBuffer.numItems = 4;
 }
 
-var rTriangle = 0;
-var rSquare = 0;
+var camera_angle_horiz = 30, camera_angle_vert = 10;
+
 
 function drawScene() {
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
@@ -71,28 +53,9 @@ function drawScene() {
     perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0);
 
     loadIdentity();
-    mvTranslate([-1.5, 0.0, -7.0]);
-    mvPushMatrix();
-    mvRotate(rTriangle, [0, 1, 0]);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
-    gl.vertexAttribPointer(
-	shaderProgram.vertexPositionAttribute,
-	triangleVertexPositionBuffer.itemSize,
-	gl.FLOAT, false, 0, 0);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexColorBuffer);
-    gl.vertexAttribPointer(
-	shaderProgram.vertexColorAttribute, triangleVertexColorBuffer.itemSize,
-	gl.FLOAT, false, 0, 0);
-
-    setMatrixUniforms();
-    gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
-    mvPopMatrix();
-    
-    mvTranslate([3.0, 0.0, 0.0]);
-    mvPushMatrix();
-    mvRotate(rSquare, [1, 0, 0]);
+    mvTranslate([0.0, 0.0, -7.0]);
+    mvRotate(camera_angle_vert, [1, 0, 0]);
+    mvRotate(camera_angle_horiz, [0, 1, 0]);
 
     gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
     gl.vertexAttribPointer(
@@ -108,23 +71,67 @@ function drawScene() {
     setMatrixUniforms();
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems);
 
-    mvPopMatrix();
-    
 }
+
+var currentlyPressedKeys = Object();
+
+var filter = 0;
+function handleKeyDown(event) {
+    currentlyPressedKeys[event.keyCode] = true;
+    if (String.fromCharCode(event.keyCode) == "F") {
+	filter += 1;
+	if (filter == 3) {
+            filter = 0;
+	}
+    }
+}
+
+function handleKeyUp(event) {
+    currentlyPressedKeys[event.keyCode] = false;
+}
+
+
+var camera_angle_shift = 0.5;
+
+function handleKeys() {
+    if (currentlyPressedKeys[KeyEvent.DOM_VK_PAGE_UP]) {
+
+    }
+    if (currentlyPressedKeys[KeyEvent.DOM_VK_PAGE_DOWN]) {
+
+    }
+    if (currentlyPressedKeys[37]) {
+	// Left cursor key
+	camera_angle_horiz += camera_angle_shift;
+    }
+    if (currentlyPressedKeys[39]) {
+	// Right cursor key
+	camera_angle_horiz -= camera_angle_shift;
+    }
+    if (currentlyPressedKeys[38]) {
+	// Up cursor key
+	camera_angle_vert += camera_angle_shift;
+    }
+    if (currentlyPressedKeys[40]) {
+	// Down cursor key
+	camera_angle_vert -= camera_angle_shift;
+    }
+}
+
 
 var lastTime = 0;
 function animate() {
     var timeNow = new Date().getTime();
     if (lastTime != 0) {
 	var elapsed = timeNow - lastTime;
-	rTriangle += (90 * elapsed) / 1000.0;
-	rSquare += (75 * elapsed) / 1000.0;
+	
     }
     lastTime = timeNow;
 }
 
 
 function tick() {
+    handleKeys();
     drawScene();
     animate();
 }
@@ -265,4 +272,126 @@ function setMatrixUniforms() {
 	shaderProgram.pMatrixUniform, false, new Float32Array(pMatrix.flatten()));
     gl.uniformMatrix4fv(
 	shaderProgram.mvMatrixUniform, false, new Float32Array(mvMatrix.flatten()));
-  }
+}
+
+// key events
+
+if (typeof KeyEvent == "undefined") {
+    var KeyEvent = {
+        DOM_VK_CANCEL: 3,
+        DOM_VK_HELP: 6,
+        DOM_VK_BACK_SPACE: 8,
+        DOM_VK_TAB: 9,
+        DOM_VK_CLEAR: 12,
+        DOM_VK_RETURN: 13,
+        DOM_VK_ENTER: 14,
+        DOM_VK_SHIFT: 16,
+        DOM_VK_CONTROL: 17,
+        DOM_VK_ALT: 18,
+        DOM_VK_PAUSE: 19,
+        DOM_VK_CAPS_LOCK: 20,
+        DOM_VK_ESCAPE: 27,
+        DOM_VK_SPACE: 32,
+        DOM_VK_PAGE_UP: 33,
+        DOM_VK_PAGE_DOWN: 34,
+        DOM_VK_END: 35,
+        DOM_VK_HOME: 36,
+        DOM_VK_LEFT: 37,
+        DOM_VK_UP: 38,
+        DOM_VK_RIGHT: 39,
+        DOM_VK_DOWN: 40,
+        DOM_VK_PRINTSCREEN: 44,
+        DOM_VK_INSERT: 45,
+        DOM_VK_DELETE: 46,
+        DOM_VK_0: 48,
+        DOM_VK_1: 49,
+        DOM_VK_2: 50,
+        DOM_VK_3: 51,
+        DOM_VK_4: 52,
+        DOM_VK_5: 53,
+        DOM_VK_6: 54,
+        DOM_VK_7: 55,
+        DOM_VK_8: 56,
+        DOM_VK_9: 57,
+        DOM_VK_SEMICOLON: 59,
+        DOM_VK_EQUALS: 61,
+        DOM_VK_A: 65,
+        DOM_VK_B: 66,
+        DOM_VK_C: 67,
+        DOM_VK_D: 68,
+        DOM_VK_E: 69,
+        DOM_VK_F: 70,
+        DOM_VK_G: 71,
+        DOM_VK_H: 72,
+        DOM_VK_I: 73,
+        DOM_VK_J: 74,
+        DOM_VK_K: 75,
+        DOM_VK_L: 76,
+        DOM_VK_M: 77,
+        DOM_VK_N: 78,
+        DOM_VK_O: 79,
+        DOM_VK_P: 80,
+        DOM_VK_Q: 81,
+        DOM_VK_R: 82,
+        DOM_VK_S: 83,
+        DOM_VK_T: 84,
+        DOM_VK_U: 85,
+        DOM_VK_V: 86,
+        DOM_VK_W: 87,
+        DOM_VK_X: 88,
+        DOM_VK_Y: 89,
+        DOM_VK_Z: 90,
+        DOM_VK_CONTEXT_MENU: 93,
+        DOM_VK_NUMPAD0: 96,
+        DOM_VK_NUMPAD1: 97,
+        DOM_VK_NUMPAD2: 98,
+        DOM_VK_NUMPAD3: 99,
+        DOM_VK_NUMPAD4: 100,
+        DOM_VK_NUMPAD5: 101,
+        DOM_VK_NUMPAD6: 102,
+        DOM_VK_NUMPAD7: 103,
+        DOM_VK_NUMPAD8: 104,
+        DOM_VK_NUMPAD9: 105,
+        DOM_VK_MULTIPLY: 106,
+        DOM_VK_ADD: 107,
+        DOM_VK_SEPARATOR: 108,
+        DOM_VK_SUBTRACT: 109,
+        DOM_VK_DECIMAL: 110,
+        DOM_VK_DIVIDE: 111,
+        DOM_VK_F1: 112,
+        DOM_VK_F2: 113,
+        DOM_VK_F3: 114,
+        DOM_VK_F4: 115,
+        DOM_VK_F5: 116,
+        DOM_VK_F6: 117,
+        DOM_VK_F7: 118,
+        DOM_VK_F8: 119,
+        DOM_VK_F9: 120,
+        DOM_VK_F10: 121,
+        DOM_VK_F11: 122,
+        DOM_VK_F12: 123,
+        DOM_VK_F13: 124,
+        DOM_VK_F14: 125,
+        DOM_VK_F15: 126,
+        DOM_VK_F16: 127,
+        DOM_VK_F17: 128,
+        DOM_VK_F18: 129,
+        DOM_VK_F19: 130,
+        DOM_VK_F20: 131,
+        DOM_VK_F21: 132,
+        DOM_VK_F22: 133,
+        DOM_VK_F23: 134,
+        DOM_VK_F24: 135,
+        DOM_VK_NUM_LOCK: 144,
+        DOM_VK_SCROLL_LOCK: 145,
+        DOM_VK_COMMA: 188,
+        DOM_VK_PERIOD: 190,
+        DOM_VK_SLASH: 191,
+        DOM_VK_BACK_QUOTE: 192,
+        DOM_VK_OPEN_BRACKET: 219,
+        DOM_VK_BACK_SLASH: 220,
+        DOM_VK_CLOSE_BRACKET: 221,
+        DOM_VK_QUOTE: 222,
+        DOM_VK_META: 224
+    };
+}
